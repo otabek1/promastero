0e20d19f7bab5ad0ce664124e012e2369e352fca
GrandHunter <grand007hunter@gmail.com>
2021-12-05 05:58:55 -0500

    final commit

--- lib/pages/order_screen.dart
+++ lib/pages/order_screen.dart
@@ -1,11 +1,37 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class OrderScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class OrderScreen extends StatefulWidget {
   var path;
   var title;
+
   OrderScreen(this.path, this.title);
+
+  @override
+  State<OrderScreen> createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  var location;
+  var _formkey = GlobalKey<FormState>();
+  var _nameController = TextEditingController();
+  var _phoneController = TextEditingController();
+  var _descriptionController = TextEditingController();
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _phoneController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +42,7 @@ class OrderScreen extends StatelessWidget {
           children: [
             Container(
               margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-              child: Text(title,
+              child: Text(widget.title,
                   style: TextStyle(
                     fontSize: 30,
                     fontWeight: FontWeight.bold,
@@ -29,7 +55,7 @@ class OrderScreen extends StatelessWidget {
             Container(
               margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
               child: Text(
-                'Order Details',
+                "${Random().nextInt(100).toString()} ta bosh mutaxassis",
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
@@ -37,9 +63,194 @@ class OrderScreen extends StatelessWidget {
                 ),
               ),
             ),
+            SizedBox(
+              height: 20,
+            ),
+            // textfield
+            Form(
+              key: _formkey,
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your name';
+                        }
+                        return null;
+                      },
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Sizning ismingiz',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your phone number';
+                        }
+                        return null;
+                      },
+                      controller: _phoneController,
+                      maxLength: 9,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Sizning telefon nomeringiz',
+                        prefix: Text('+998'),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your description';
+                        }
+                        return null;
+                      },
+                      controller: _descriptionController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Muammoni qisaqcha yozing',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton.icon(
+                    icon: Icon(Icons.location_pin),
+                    label: Text("Lokatsiyani tanlang"),
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      fixedSize: Size(290, 55),
+                      primary: Color.fromRGBO(0, 227, 173, 1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    onPressed: () async {
+                      if (_formkey.currentState!.validate()) {
+                        location = await _determinePosition();
+                        if (location != null) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                "Lokatsiya tanlandi",
+                              ),
+                            ),
+                          );
+                        }
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      fixedSize: Size(290, 55),
+                      primary: Color.fromRGBO(0, 227, 173, 1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    onPressed: () async {
+                      if (_formkey.currentState!.validate()) {
+                        try {
+                          await firestore.collection('orders').add({
+                            'name': _nameController.text,
+                            'phone': _phoneController.text,
+                            'location': location.toString(),
+                            'desc': _descriptionController.text,
+                          });
+                          // show dialog
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text("Tayyor"),
+                                content: Text(
+                                    "Sizning so'rovni bajarildi. Sizga javob beriladi"),
+                                actions: [
+                                  FlatButton(
+                                    child: Text("OK"),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          ).then((value) => Navigator.of(context).pop());
+                        } catch (e) {
+                          print(e);
+                        }
+                      }
+                    },
+                    child: Text(
+                      "Yuborish",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
 }
